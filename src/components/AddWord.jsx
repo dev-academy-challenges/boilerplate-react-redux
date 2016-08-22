7e3f267cb82010c933efae4a1cd7fdbc74326c2d
Rich Churcher <rich.churcher@gmail.com>
2016-08-23 08:57:17 +1200

    Clean up a little

--- src/components/AddWord.jsx
+++ src/components/AddWord.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {addWord} from '../actions'
 
+function submitWord (e, dispatch) {
+  if (e.keyCode === 13) {
+    dispatch(addWord(e.currentTarget.value))
+    e.currentTarget.value = ''
+  }
+}
+
 let AddWord = ({dispatch}) => (
-  <input onKeyUp={e => {
-    if (e.keyCode === 13) {
-      dispatch(addWord(e.currentTarget.value))
-      e.currentTarget.value = ''
-    }
-  }}/>
+  <input
+    placeholder="Enter a word or phrase"
+    onKeyUp={e => {
+      submitWord(e, dispatch)
+    }}
+    />
 )
 
 AddWord = connect()(AddWord)
