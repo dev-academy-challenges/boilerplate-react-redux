d2f4fd362d42b4b6eb9c7db47b2e272c39335a2a
Don Smith <hi@dons.dev>
2020-06-28 22:04:29 +1200

    move public folder into server folder

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
-server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.static(path.join(__dirname, 'public')))
 
 module.exports = server
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.join(__dirname, '/public'),
+    path: path.join(__dirname, 'server/public'),
     filename: 'bundle.js'
   },
   mode: 'development',
